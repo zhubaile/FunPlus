8b0db0fcaf4af92a5b5bf2c9c80cabbb48299a37
zhubaile <1367050904@qq.com>
2019-04-26 16:59:45 +0800

    修改了'国际化'配置信息，添加了Login里面的注册界面，axios配置使用

---
+++ src/apps/Internationalization/locales/en-US/Login/login.js
@@ -0,0 +1,24 @@
+/*
+* 登录界面的语言
+* */
+export default {
+  // 登录界面
+  'app.login.sign.in': 'login',
+  'app.login.user.errormessage': 'Mailbox input error',
+  'app.login.pass.errormessage': 'Password Input Error',
+  'app.login.storage.number': 'Remember account number',
+  'app.login.register.now': 'Register Now',
+
+  // 发送注册界面
+  'app.register.register': 'register',
+  'app.sendregister.send': 'send',
+  'app.sendregister.information': 'FunPlus will send a validation email to your mailbox, which will be used as the login user name, click the button, and register the account.',
+  'app.sendregister.user.errormessage': 'Please enter the correct mailbox',
+  'app.sendregister.newaccount': 'Log in with an existing account',
+
+  // 注册界面
+  'app.register.user.errorname': 'Please enter the correct user name',
+  'app.register.user.errormailbox': 'Please enter the correct mailbox',
+  'app.register.user.errorpass': '请输入正确的密码',
+  'app.register.confirm password': '请确保两次密码一致',
+};
---
+++ src/apps/Internationalization/locales/zh-CN/Login/login.js
@@ -0,0 +1,24 @@
+/*
+* 登录界面的语言
+* */
+export default {
+  // 登录界面
+  'app.login.sign.in': '登录',
+  'app.login.user.errormessage': '邮箱输入错误',
+  'app.login.pass.errormessage': '密码输入错误',
+  'app.login.storage.number': '记住账号',
+  'app.login.register.now': '立即注册',
+
+  // 发送注册界面
+  'app.register.register': '注册',
+  'app.sendregister.send': '发送',
+  'app.sendregister.information': 'FunPlus 将发送一封验证邮件到你的邮箱，此邮箱将作为登录用户名，点击按钮，注册账号',
+  'app.sendregister.user.errormessage': '请输入正确的邮箱',
+  'app.sendregister.newaccount': '使用已有账户登录',
+
+  // 注册界面
+  'app.register.user.errorname': '请输入正确的用户名',
+  'app.register.user.errormailbox': '请输入正确的邮箱',
+  'app.register.user.errorpass': '请输入正确的密码',
+  'app.register.confirm password': '请确保两次密码一致',
+};
