1777cab54d20788a6a533d3db48f35046db0c054
zhubaile <1367050904@qq.com>
2019-06-06 17:59:59 +0800

    设备管理接口对接

--- src/apps/index/api/index.js
+++ src/apps/index/api/index.js
@@ -13,16 +13,36 @@ export async function getMenu(params) {
   //   ;
   // });deviceGroup
 }
+// 获取分组列表
+export async function deviceGrouplist() {
+  return ajaxAmd.get({
+    url: '/deviceGroup/list',
+  });
+}
 // 获取设备组内容
 export async function deviceGroupaddparmas() {
   return ajaxAmd.get({
     url: '/deviceGroup/add/parmas',
   });
 }
-// 设备组接口
+// 添加设备组接口
 export async function deviceGroupadd(params) {
   return ajaxAmd.post({
-    url: '/deviceGroup/add ',
+    url: '/deviceGroup/add',
+    data: params,
+  });
+}
+// 获取添加设备参数
+export async function deviceparams(params) {
+  return ajaxAmd.get({
+    url: '/device/params',
+    params,
+  });
+}
+// 添加设备
+export async function deviceadd(params) {
+  return ajaxAmd.post({
+    url: '/device/add',
     data: params,
   });
 }
